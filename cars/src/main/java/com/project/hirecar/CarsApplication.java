7c13e963c2beb738ccd5756cecb6183ac031d305
saadelbannani <saadelbannani>
2020-01-26 14:21:31 +0100

    Creation of discovery server for registration

---
+++ cars/src/main/java/com/project/hirecar/CarsApplication.java
@@ -0,0 +1,17 @@
+package com.project.hirecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAutoConfiguration
+public class CarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsApplication.class, args);
+    }
+
+}
